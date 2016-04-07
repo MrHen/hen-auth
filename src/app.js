df424359123d1401b758134e786c9e618f7f7eeb
Adam Babcock <adam.babcock@smartsocialmedia.com>
2016-04-07 11:04:59 -0500

    Serve the public files

--- src/app.js
+++ src/app.js
@@ -39,7 +39,6 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieParser());
 
-
 app.use('/api/', routes);
 app.use('/api/users', users);
 app.use('/api/secured', authenticate);
@@ -48,12 +47,12 @@ app.get('/api/ping', function(req, res) {
   res.send("All good. You don't need to be authenticated to call this");
 });
 
-
 app.get('/api/secured/ping', function(req, res) {
   res.status(200).send(
     "All good. You only get this message if you're authenticated");
 });
 
 app.use('/sample', express.static(path.join(__dirname, 'sample')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 module.exports = app;
