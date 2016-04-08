db9eefe9159216fa0964bd3ffbf3985a1abf7642
Adam Babcock <adam.babcock@smartsocialmedia.com>
2016-04-08 15:29:40 -0500

    Formatting

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -9,29 +9,29 @@ let router = express.Router();
 router.use("/secured", authenticate.token());
 router.use("/scoped", authenticate.token());
 // router.use("/scoped", authenticate.userValues({"app_metadata.test": "test"}));
-router.use("/scoped", permissions({resource: "test"}));
+router.use("/scoped", permissions({ resource: "test" }));
 
 router.use("/public", express.static(path.join(__dirname, "public")));
 
 router.get("/ping", function(req, res) {
-  res.send("All good. You don't need to be authenticated to call this");
+    res.send("All good. You don't need to be authenticated to call this");
 });
 
 router.get("/secured/ping", function(req, res) {
-  res.status(200).send(
-    "All good. You only get this message if you're authenticated");
+    res.status(200).send(
+        "All good. You only get this message if you're authenticated");
 });
 
 router.get("/scoped/ping", function(req, res) {
-  res.status(200).send(
-    "All good. You only get this message if you're scoped");
+    res.status(200).send(
+        "All good. You only get this message if you're scoped");
 });
 
 router.get("/", function(req, res, next) {
-  res.render("index", {
-    rootUrl: ".",
-    title: "Express"
-  });
+    res.render("index", {
+        rootUrl: ".",
+        title: "Express"
+    });
 });
 
 module.exports = router;
