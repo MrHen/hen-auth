39e7a1b4c4eea2e70176eed94e3e93d6e5fc2c4d
Adam Babcock <adam.babcock@smartsocialmedia.com>
2016-04-08 10:58:00 -0500

    Formatting

--- src/server.ts
+++ src/server.ts
@@ -12,69 +12,68 @@ import logger = require("morgan");
 
 let apiRoute = require("./api/routes/index");
 
-dotenv
-  .config({
+dotenv.config({
     silent: true
-  });
+});
 
 async.auto({
-  "app": (cb) => {
-    let app = express();
+    "app": (cb) => {
+        let app = express();
 
-    let port = process.env.PORT || 3000;
-    app.set("port", port);
+        let port = process.env.PORT || 3000;
+        app.set("port", port);
 
-    app.use(cors());
+        app.use(cors());
 
-    app.set("views", path.join(__dirname, "api", "views"));
-    app.set("view engine", "jade");
+        app.set("views", path.join(__dirname, "api", "views"));
+        app.set("view engine", "jade");
 
-    app.use(logger("dev"));
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({
-      extended: false
-    }));
-    // app.use(cookieParser());
+        app.use(logger("dev"));
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({
+            extended: false
+        }));
+        // app.use(cookieParser());
 
-    cb(null, app);
-  },
-  "routes": ["app", (results, cb) => {
-    let authenticate = expressJwt({
-      secret: new Buffer(process.env.AUTH0_CLIENT_SECRET, "base64"),
-      audience: process.env.AUTH0_CLIENT_ID
-    });
+        cb(null, app);
+    },
+    "routes": ["app", (results, cb) => {
+        let authenticate = expressJwt({
+            secret: new Buffer(process.env.AUTH0_CLIENT_SECRET, "base64"),
+            audience: process.env.AUTH0_CLIENT_ID
+        });
 
-    results.app.use("/api/", apiRoute);
-    results.app.use("/api/secured", authenticate);
-    results.app.use("/api/scoped", authenticate);
-    results.app.use("/api/scoped", function(req, res, next) {
-      if (!req.user || !req.user.app_metadata) {
-        return res.status(403).send("No app_metadata.");
-      }
+        results.app.use("/api/", apiRoute);
+        results.app.use("/api/secured", authenticate);
+        results.app.use("/api/scoped", authenticate);
+        results.app.use("/api/scoped", function(req, res, next) {
+            if (!req.user || !req.user.app_metadata) {
+                return res.status(403).send("No app_metadata.");
+            }
 
-      if (req.user.app_metadata.test !== "test") {
-        return res.status(403).send("Missing claim.");
-      }
+            if (req.user.app_metadata.test !== "test") {
+                return res.status(403).send("Missing claim.");
+            }
 
-      next();
-    });
+            next();
+        });
 
-    results.app.use("/bower_components", express.static(path.join(__dirname,
-      "bower_components")));
-    results.app.use("/public", express.static(path.join(__dirname, "public")));
-    results.app.use("/", express.static(path.join(__dirname, "sample")));
+        results.app.use("/bower_components", express.static(path.join(__dirname,
+            "bower_components")));
+        results.app.use("/public", express.static(path.join(__dirname, "public")));
+        results.app.use("/", express.static(path.join(__dirname, "sample")));
 
-    cb();
-  }],
-  "server": ["app", "routes", (results, cb) => {
-    cb(null, http.createServer(results.app));
-  }],
-  "listen": ["server", (results, cb) => {
-      results.server.listen(results.app.get("port"), () => {
-          console.info("Express server listening", {port: results.app.get("port")});
-          cb(null);
-      });
-  }],
+        cb();
+    }],
+    "server": ["app", "routes", (results, cb) => {
+        cb(null, http.createServer(results.app));
+    }],
+    "listen": ["server", (results, cb) => {
+        results.server.listen(results.app.get("port"), () => {
+            console.info("Express server listening", { port: results.app.get("port") });
+            cb(null);
+        });
+    }],
 }, (err: Error, result) => {
-  console.info("Setup completed", {err: err});
+    console.info("Setup completed", { err: err });
 });
