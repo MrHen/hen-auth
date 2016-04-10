c3e61eb8f41e0f4df9a3ca67d821b108372a1e9e
Adam Babcock <adam.babcock@smartsocialmedia.com>
2016-04-10 13:21:15 -0500

    Pull logic out of angular setup

--- src/dashboard/home/home.ts
+++ src/dashboard/home/home.ts
@@ -4,60 +4,64 @@ namespace DashboardHome {
             "auth0",
             "ui.router"
         ])
-        .config(function myAppConfig($stateProvider) {
-            $stateProvider
-                .state("home", {
-                    url: "/",
-                    templateUrl: "home/home.html",
-                    controller: "HomeCtrl",
-                    data: {
-                        requiresLogin: true
-                    }
-                });
-        })
-        .controller("HomeCtrl", function HomeController($scope, auth, $http,
-            $location, store) {
+        .config(dashboardHomeConfig)
+        .controller("HomeCtrl", HomeController);
 
-            $scope.auth = auth;
+    function dashboardHomeConfig($stateProvider) {
+        $stateProvider
+            .state("home", {
+                url: "/",
+                templateUrl: "home/home.html",
+                controller: "HomeCtrl",
+                data: {
+                    requiresLogin: true
+                }
+            });
+    }
 
-            $scope.callApi = function() {
-                // Just call the API as you'd do using $http
-                $http({
-                    url: "/api/secured/ping",
-                    method: "GET"
-                }).then(function(result) {
-                    $scope.response = JSON.stringify(result, null, 2);
-                }, function(response) {
-                    if (response.status === -1) {
-                        alert("Please download the API seed so that you can call it.");
-                    } else {
-                        alert(response.data);
-                    }
-                });
-            };
+    function HomeController($scope, auth, $http,
+        $location, store) {
 
-            $scope.callScopedApi = function() {
-                // Just call the API as you'd do using $http
-                $http({
-                    url: "/api/scoped/ping",
-                    method: "GET"
-                }).then(function(result) {
-                    $scope.response = JSON.stringify(result, null, 2);
-                }, function(response) {
-                    if (response.status === -1) {
-                        alert("Please download the API seed so that you can call it.");
-                    } else {
-                        alert(response.data);
-                    }
-                });
-            };
+        $scope.auth = auth;
 
-            $scope.logout = function() {
-                auth.signout();
-                store.remove("profile");
-                store.remove("token");
-                $location.path("/login");
-            };
+        $scope.callApi = function() {
+            // Just call the API as you'd do using $http
+            $http({
+                url: "/api/secured/ping",
+                method: "GET"
+            }).then(function(result) {
+                $scope.response = JSON.stringify(result, null, 2);
+            }, function(response) {
+                if (response.status === -1) {
+                    alert("Please download the API seed so that you can call it.");
+                } else {
+                    alert(response.data);
+                }
+            });
+        };
 
-        });
+        $scope.callScopedApi = function() {
+            // Just call the API as you'd do using $http
+            $http({
+                url: "/api/scoped/ping",
+                method: "GET"
+            }).then(function(result) {
+                $scope.response = JSON.stringify(result, null, 2);
+            }, function(response) {
+                if (response.status === -1) {
+                    alert("Please download the API seed so that you can call it.");
+                } else {
+                    alert(response.data);
+                }
+            });
+        };
+
+        $scope.logout = function() {
+            auth.signout();
+            store.remove("profile");
+            store.remove("token");
+            $location.path("/login");
+        };
+
+    }
 }
