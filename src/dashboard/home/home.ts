7a7b319316a9acd2c32b252278c7ac3050e6b45b
Adam Babcock <adam.babcock@smartsocialmedia.com>
2016-04-10 13:18:15 -0500

    Convert home.js to TypeScript

--- src/dashboard/home/home.ts
+++ src/dashboard/home/home.ts
@@ -1,19 +1,19 @@
-angular.module('dashboard.home', [
-    'auth0',
-    'ui.router'
+angular.module("dashboard.home", [
+    "auth0",
+    "ui.router"
   ])
   .config(function myAppConfig($stateProvider) {
     $stateProvider
-      .state('home', {
+      .state("home", {
         url: "/",
         templateUrl: "home/home.html",
-        controller: 'HomeCtrl',
+        controller: "HomeCtrl",
         data: {
           requiresLogin: true
         }
       });
   })
-  .controller('HomeCtrl', function HomeController($scope, auth, $http,
+  .controller("HomeCtrl", function HomeController($scope, auth, $http,
     $location, store) {
 
     $scope.auth = auth;
@@ -21,12 +21,12 @@ angular.module('dashboard.home', [
     $scope.callApi = function() {
       // Just call the API as you'd do using $http
       $http({
-        url: '/api/secured/ping',
-        method: 'GET'
+        url: "/api/secured/ping",
+        method: "GET"
       }).then(function(result) {
         $scope.response = JSON.stringify(result, null, 2);
       }, function(response) {
-        if (response.status == -1) {
+        if (response.status === -1) {
           alert("Please download the API seed so that you can call it.");
         } else {
           alert(response.data);
@@ -37,12 +37,12 @@ angular.module('dashboard.home', [
     $scope.callScopedApi = function() {
       // Just call the API as you'd do using $http
       $http({
-        url: '/api/scoped/ping',
-        method: 'GET'
+        url: "/api/scoped/ping",
+        method: "GET"
       }).then(function(result) {
         $scope.response = JSON.stringify(result, null, 2);
       }, function(response) {
-        if (response.status == -1) {
+        if (response.status === -1) {
           alert("Please download the API seed so that you can call it.");
         } else {
           alert(response.data);
@@ -52,9 +52,9 @@ angular.module('dashboard.home', [
 
     $scope.logout = function() {
       auth.signout();
-      store.remove('profile');
-      store.remove('token');
-      $location.path('/login');
+      store.remove("profile");
+      store.remove("token");
+      $location.path("/login");
     };
 
   });
