f9e273739dc2604db47b3e67578aac3a78c8792e
Adam Babcock <adam.babcock@smartsocialmedia.com>
2016-04-10 13:32:38 -0500

    Move logic out of angular setup

--- src/dashboard/login/login.ts
+++ src/dashboard/login/login.ts
@@ -1,26 +1,30 @@
-namespace DashboardHome {
+namespace DashboardLogin {
     angular.module("dashboard.login",
         [
             "auth0",
             "ui.router"
         ])
-        .config(function myAppConfig($stateProvider) {
-            $stateProvider
-                .state("login", {
-                    url: "/login",
-                    templateUrl: "login/login.html",
-                    controller: "LoginCtrl"
-                });
-        })
-        .controller("LoginCtrl", function($scope, auth) {
+        .config(dashboardLoginConfig)
+        .controller("LoginCtrl", LoginController);
 
-            $scope.signin = function() {
-                auth.signin({
-                    authParams: {
-                        scope: "openid name email app_metadata" // Specify the scopes you want to retrieve
-                    }
-                });
-            };
+    function dashboardLoginConfig($stateProvider) {
+        $stateProvider
+            .state("login", {
+                url: "/login",
+                templateUrl: "login/login.html",
+                controller: "LoginCtrl"
+            });
+    }
 
-        });
+    function LoginController($scope, auth) {
+
+        $scope.signin = function() {
+            auth.signin({
+                authParams: {
+                    scope: "openid name email app_metadata" // Specify the scopes you want to retrieve
+                }
+            });
+        };
+
+    }
 }
